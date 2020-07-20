6b13818152544f24387ec82809bcd5a22b0c5624
301083645 <67066443+301083645@users.noreply.github.com>
2020-07-20 10:49:04 -0400

    Assignment4 Start with some of codes

--- WindowsFormsApp1/BMICalculatorForm.Designer.cs
+++ WindowsFormsApp1/BMICalculatorForm.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // BMICalculatorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -38,8 +38,9 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "BMICalculatorForm";
+            this.Text = "Calculator Form";
+            this.Load += new System.EventHandler(this.BMICalculatorForm_Load);
             this.ResumeLayout(false);
 
         }
--- WindowsFormsApp1/BMICalculatorForm.cs
+++ WindowsFormsApp1/BMICalculatorForm.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void BMICalculatorForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
